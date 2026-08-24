d5090c4d9bddbadb3425d3316c4b8257d93aa422
Bruno Dias <Brunodiassilva1195@gmail.com>
2026-08-24 17:26:35 -0300

    Update TesteSelenium.py

--- TesteSelenium.py
+++ TesteSelenium.py
@@ -1,10 +1,10 @@
 import time
 import subprocess
+from unittest import result
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.edge.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.service import Service
@@ -14,11 +14,11 @@ from selenium.webdriver.chrome.options import Options
 def main():
 
     # Inicia o Chrome
-    chrome_options = Options()
-    chrome_options.debugger_address = "127.0.0.1:9222"
-    driver = webdriver.Chrome(options=chrome_options)
-    #--------------------Tela de login---------------------
+    service = Service(executable_path="C:\\Users\\2160044544\\OneDrive - Grupo Casas Bahia S.A\\Área de Trabalho\\AutoPontuacao\\chromedriver-win64\\chromedriver.exe")
+    driver = webdriver.Chrome(service=service)
 
+    #--------------------Tela de login---------------------
+    
     # 1.1. Acessar a página de login
     driver.get("http://admin.casasbahia.com.br/Site/Login.aspx")
 
@@ -61,22 +61,44 @@ def main():
     # 3.2. Preenche o campo de compra com o número do pedido
     WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.ID, "ctl00_Conteudo_btnFiltrar")))
     campo_pedido = driver.find_element(By.ID, "ctl00_Conteudo_tbxIdCompra_txtId")
-    campo_pedido.send_keys("508425409")
+    campo_pedido.send_keys("520316880")
 
     # 3.3. Clica no botão de pesquisar
     botao_pesquisar = driver.find_element(By.ID, "ctl00_Conteudo_btnFiltrar")
     botao_pesquisar.click()
 
-    #--------------------Validação de Elegibilidade---------------------
-
+    # 3.4. Clica no link do pedido para abrir a tela de detalhes
     WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.ID, "ctl00_Conteudo_rptCompra_ctl01_lnkCompraDetalhe")))
     Click_pedido = driver.find_element(By.ID, "ctl00_Conteudo_rptCompra_ctl01_lnkCompraDetalhe")
     Click_pedido.click()
 
+    #--------------------Validação de Elegibilidade---------------------
+
+    WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.ID, "ctl00_Conteudo_Tabs_Panel1_lblParceiro")))
+
+    # 4.1. Valida o parceiro
+    Parceiro = driver.find_element(By.ID, "ctl00_Conteudo_Tabs_Panel1_lblParceiro")
+    Parceiro_texto = Parceiro.text.strip()
+
+    # 4.2. Valida a midia
+    Midia = driver.find_element(By.ID, "ctl00_Conteudo_Tabs_Panel1_lblMidia")
+    Midia_texto = Midia.text.strip()
+
+    # 4.3. Valida a campanha
+    Campanha = driver.find_element(By.ID, "ctl00_Conteudo_Tabs_Panel1_lblCampanha")
+    Campanha_texto = Campanha.text.strip()
+
+    if Parceiro_texto == "b2b_livelo" and Midia_texto == "livelo" and Campanha_texto == "livelo_acumulo":
+        fluxo_correto("Elegível")
+        print("\nElegível")
+
+    else:
+        fluxo_divergente("Não Elegível")
+        print("\nNão Elegível")
+   
 
 
-
-    input("Pressione ENTER para fechar o navegador...")
+    input("\nPressione ENTER para fechar o navegador...")
     driver.quit()
 
 
